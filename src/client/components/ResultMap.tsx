e8889e56e4344840a030587a825178dea26fef9e
TwentyFive <TwentyFiveSoftware@gmail.com>
2025-10-29 20:23:14 +0100

    Add fit-bounds to markers on leaflet maps

--- src/client/components/ResultMap.tsx
+++ src/client/components/ResultMap.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 type Props = {
     elements: OSMNode[];
@@ -41,11 +41,28 @@ export const ResultMap: React.FC<Props> = ({ elements }) => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
 
-            {elements.map(element => (
-                <Marker position={[element.lat, element.lon]} key={element.id}>
+            <Markers markers={elements} />
+        </MapContainer>
+    );
+};
+
+const Markers: React.FC<{ markers: OSMNode[] }> = ({ markers }) => {
+    const map = useMap();
+
+    useEffect(() => {
+        map.fitBounds(
+            markers.map(marker => [marker.lat, marker.lon]),
+            { padding: [30, 30] },
+        );
+    }, [map, markers]);
+
+    return (
+        <>
+            {markers.map(marker => (
+                <Marker key={marker.id} position={[marker.lat, marker.lon]}>
                     <Popup>
                         <ul>
-                            {Object.entries(element.tags).map(([key, value]) => (
+                            {Object.entries(marker.tags).map(([key, value]) => (
                                 <li key={key}>
                                     <b>{key}</b>: {value}
                                 </li>
@@ -54,6 +71,6 @@ export const ResultMap: React.FC<Props> = ({ elements }) => {
                     </Popup>
                 </Marker>
             ))}
-        </MapContainer>
+        </>
     );
 };
