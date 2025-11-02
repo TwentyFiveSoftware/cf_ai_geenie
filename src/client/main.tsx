4fdc09596797dee8c97bf57a99d6797c0ac8427a
TwentyFive <TwentyFiveSoftware@gmail.com>
2025-11-02 15:28:21 +0100

    Fix leaflet markers not loading

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,6 +4,15 @@ import { App } from './components/App.tsx';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
 
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Marker.prototype.options.icon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+});
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
